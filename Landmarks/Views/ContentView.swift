1fe5fc403ed5078a6f0cfe70c75a7a1881626962
chang8128 <chang8128@sina.cn>
2022-11-01 17:22:38 +0800

    update view

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 常志平 on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
